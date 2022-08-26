bcdf9af91c78a6bdc9cdb8d7d695c5716aac0594
Aakash Rathore <aakash.rathore321@gmail.com>
2022-08-26 17:10:28 +0530

    completed forgot password functionality

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import {Navigate, Outlet} from 'react-router-dom' 
+import {useAuthStatus} from '../hooks/useAuthStatus'
+import { Spinner } from './Spinner'
 
 
 const PrivateRoute = () => {
 
-    const loggedIn = true
+    const {loggedIn, checkingStatus} = useAuthStatus();
+
+    if(checkingStatus){
+      return <Spinner />
+    }
 
   return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />
   
